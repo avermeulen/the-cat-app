00e38d6b522161bf8015b8d0aefb2d9fe4a15a7c
avermeulen <andre.vermeulen@gmail.com>
2023-07-18 16:46:06 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/happy.png">
+  <HelloWorld msg="Welcome to the cat App"/>
+  <a href="https://www.flaticon.com/free-icons/cute" title="cute icons">Cute icons created by Freepik - Flaticon</a>
+  <a href="https://www.flaticon.com/free-icons/cat" title="cat icons">Cat icons created by Smashicons - Flaticon</a>
 </template>
 
 <script>
